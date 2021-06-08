2c28041f0e40e2967b0d79055d4451cd669a9c49
hawkpf <hawkpf@miamioh.edu>
2021-06-08 11:38:31 -0400

    Update instance.pp

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -42,6 +42,7 @@ define mediawiki::instance (
   $db_name                = $name,
   $db_user                = "${name}_user",
   $db_prefix              = 'wk',
+  $db_admin_pass          = undef,
   $ip                     = '*',
   $port                   = '80',
   $sslport                = '443',
@@ -65,6 +66,10 @@ define mediawiki::instance (
   validate_re($ensure, '^(present|absent|deleted)$',
   "${ensure} is not supported for ensure.
   Allowed values are 'present', 'absent', and 'deleted'.")
+  
+  if ($db_admin_pass == undef) or (length($db_admin_pass) < 10){
+      fail('db_admin_pass is undefined or it must be be at least 10 characters long.')
+  }
 
   include ::mediawiki::params
 
@@ -99,7 +104,7 @@ define mediawiki::instance (
       exec { "${name}-install_script":
         cwd       => "${mediawiki_install_path}/maintenance",
         command   => "/usr/bin/php install.php                   \
-                        --pass puppet05                          \
+                        --pass ${db_admin_pass}                  \
                         --server http://${server_name}           \
                         --scriptpath /${name}                    \
                         --dbtype mysql                           \
